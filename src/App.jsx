02dc0e3af227fba72453fb67043301e6fa267222
alejandro <alejandroziegler18@gmail.com>
2024-01-29 18:16:29 -0300

    segunda carga

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import second from './components/navBar';
+
+function App() {
+  return (
+    <navBar />
+  );
+}
+
+export default App;
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const navBar = () => {
+    return ( 
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav__limk" href="#">MiMarca</a>
+                </div>
+                <ul className="nav__link">
+                    <li>
+                        <a className="nav__link" href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">Carrito</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+     );     
+}
+
+export default navBar;
