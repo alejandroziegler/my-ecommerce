f5e00c311b8d1360aa7ff49295c6ddbd06dd2765
alejandro <alejandroziegler18@gmail.com>
2024-01-30 03:19:03 -0300

    4 guardado

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,14 @@
+import ItemCount from "../ItemCount";
 import React from "react";
 import Title from "../Title";
 
 export const ItemListContainer = ({texto}) => {
+
     return ( 
-        <Title greeting={texto}/>
+        <>
+            <Title greeting={texto}/>
+            <ItemCount />
+        </>
     );
 }
 
